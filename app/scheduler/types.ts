446ca3c2188bfbeea1cf5a44de84239fef122e68
fullstacknyc <myemailxd@icloud.com>
2026-03-22 22:38:57 -0400

    error

---
+++ app/scheduler/types.ts
@@ -0,0 +1,13 @@
+export type TimeSession = {
+  id: string;
+  activity: string;
+  startedAt: string;
+  endedAt: string;
+};
+
+export type ActiveSession = {
+  id: string;
+  activity: string;
+  startedAt: string;
+};
+
